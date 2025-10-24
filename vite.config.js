f3ef21e99e143d92bb98a127022fec5588671bc9
Alexandre <alexandre.posta@epita.fr>
2025-10-24 15:50:04 +0200

    a

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    strictPort: false
+    strictPort: false,
+    proxy: {
+      '/api': {
+        target: 'http://backend:8000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 })
